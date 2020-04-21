e7f64f42182dfe5e6546771afe75cf504db1a19f
gabeaster <gab.easter@live.com>
2020-04-21 17:42:08 -0400

    utils folder setup with privateroute and axiosWithAux; App.js Router changed to Switch

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1>Here we go</h1>
+      <Switch>
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   );
 }
